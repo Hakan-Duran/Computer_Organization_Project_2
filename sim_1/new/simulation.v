e423d3e6cfa5ee83e9064ebb09359f4ec9f1486a
Muhammed Yusuf Mermer <myusufmermer@gmail.com>
2023-05-16 16:50:22 +0300

    initial commit

---
+++ sim_1/new/simulation.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.05.2023 16:43:21
+// Design Name: 
+// Module Name: simulation
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module simulation(
+
+    );
+endmodule
---
+++ sources_1/new/designs.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.05.2023 16:42:48
+// Design Name: 
+// Module Name: designs
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module designs(
+
+    );
+endmodule
